b2b8d4c134789dde3329c31d89cc945543b302a1
sushmaasaini1979-jpg <sushmaasaini1979@gmail.com>
2025-10-03 21:50:03 +0530

    Delete clean-frontend directory

--- clean-frontend/src/components/ConnectionStatus.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ConnectionStatus = () => {
-  // Hide connection status completely to prevent user confusion
-  // The socket will handle reconnection silently in the background
-  return null
-}
-
-export default ConnectionStatus
